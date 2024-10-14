e18747804bd32e0c64960c504947b4a9a819343f
Mohamed Alawady <101016792+moal223@users.noreply.github.com>
2024-10-14 20:28:42 +0200

    remove skin model

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # Load TensorFlow models
 burnDegree = BurnModel('models/purnDegree.h5')
-skinModel = SkinDisease('models/skin.h5')
+# skinModel = SkinDisease('models/skin2.h5')
 typeModel = TypeModel('models/type.pkl')
 
 
@@ -37,20 +37,20 @@ def predict():
         response_list = []
         image = Image.open(file.stream)
 
-        # typePred = typeModel.predict(image)
+        typePred = typeModel.predict(image)
 
-        # if typePred == 'burn wound':
-        #     image_process = preprocess_image(image)
-        #     response = burnDegree.predict_burn_degree(image_process)
-        #     response_list.append(response)
-        # else: 
-        #     response_list.append(typePred)
+        if typePred == 'burn wound':
+            image_process = preprocess_image(image)
+            response = burnDegree.predict_burn_degree(image_process)
+            response_list.append(response)
+        else: 
+            response_list.append(typePred)
         
         
 
-        image_skin = skinModel.preprocessing(image)
-        skin_response = skinModel.predict(image_skin)
-        response_list.append(skin_response)
+        # image_skin = skinModel.preprocessing(image)
+        # skin_response = skinModel.predict(image_skin)
+        # response_list.append(skin_response)
         
         # Return the prediction result as JSON
         return jsonify({'Output': response_list})
